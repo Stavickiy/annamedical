119e8619c1937866677db53aabe6bd2ffab57c5b
Stavickiy <stavickiydgo@gmail.com>
2024-06-07 15:32:35 +0200

    Development completed

--- annamedical/settings.py
+++ annamedical/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jne)+p@pd+$im6=8)1abpp5p2(&pm)j&w5ds$kgzd*%u^v6%nb'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -85,9 +86,9 @@ WSGI_APPLICATION = 'annamedical.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'anna_dent',
-        'USER': os.getenv('USER_DB_NAME'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'NAME': config('DB_NAME'),
+        'USER': config('USER_DB_NAME'),
+        'PASSWORD': config('DB_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
